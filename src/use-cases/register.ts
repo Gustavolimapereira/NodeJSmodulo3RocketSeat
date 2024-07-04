86a05b3bc6cd1b9af93a51b99e96e1eebc91f038
gustavolimapereira <gu.lim@hotmail.com>
2024-07-04 15:41:41 -0300

    update

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -17,7 +18,7 @@ export class RegisterUseCase {
     const userWithSameEmail = await this.userRepository.findByEmail(email);
 
     if (userWithSameEmail) {
-      throw new Error("E-mail already exists.");
+      throw new UserAlreadyExistsError();
     }
 
     // const prismaUsersRepository = new PrismaUsersRepository();
